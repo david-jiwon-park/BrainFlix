7b7fa837f8fe30bbf17b990d1e0fff5731d451f7
david-jiwon-park <davidpark48@hotmail.com>
2023-11-11 14:43:53 -0500

    Adding upload button to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import './Header.scss';
 import brainflixLogo from '../../assets/images/BrainFlix-logo.svg';
 import searchIcon from '../../assets/images/search.svg';
 import pfp from '../../assets/images/Mohan-muruge.jpg';
+import uploadIcon from '../../assets/images/upload.svg';
 
 function Header() {
 
@@ -22,6 +23,13 @@ function Header() {
                     <img className="header__search-icon" src={searchIcon} alt="search icon"/>
                 </div>
                 <img className="header__pfp" src={pfp} alt="pfp"/>
+            </div>
+
+            <div className="header__button-container">
+                <button id="upload-button" className="header__button">
+                    UPLOAD
+                </button>
+                <img className="header__upload-icon" src={uploadIcon} alt="upload icon"/>
             </div>
 
         </header>
