39b0fe0268e7005c26f148c107b0f7d0f7931aa4
david-jiwon-park <davidpark48@hotmail.com>
2023-11-25 22:22:34 -0500

    Making minor styling updates to VideoDetails section

--- src/components/VideoDetails/VideoDetails.js
+++ src/components/VideoDetails/VideoDetails.js
@@ -21,12 +21,12 @@ function VideoDetails(props) {
                 <div className="video-details__data--right"> 
                     <div className="video-details__views">
                         <img className="video-details__icon" src={viewsIcon} alt="views icon"/>
-                        <h5 className="video-details__data-text">{props.views}</h5>
+                        <h5 className="video-details__data-text video-details__data-text--right">{props.views}</h5>
                     </div>
 
                     <div className="video-details__likes">
                         <img className="video-details__icon" src={likesIcon} alt="likes icon"/>
-                        <h5 className="video-details__data-text">{props.likes}</h5>
+                        <h5 className="video-details__data-text video-details__data-text--right">{props.likes}</h5>
                     </div>
                 </div>
 
