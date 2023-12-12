8f0b242f029fbd37ef3b32aa0f3d1b2d61aa4a4a
david-jiwon-park <davidpark48@hotmail.com>
2023-12-12 03:21:44 -0500

    Creating desktop layout for Video Upload page

--- src/pages/VideoUpload/VideoUpload.js
+++ src/pages/VideoUpload/VideoUpload.js
@@ -7,48 +7,55 @@ function VideoUpload() {
 
     return (
         <div className='video-upload'>
-            <h1 className='video-upload__heading'>Upload Video</h1>
-            <h4 className='video-upload__label video-upload__label--thumbnail'>VIDEO THUMBNAIL</h4>
-            <img className='video-upload__thumbnail' src={thumbnail} alt='thumbnail'/>
-
-            <form>
-                <label htmlFor="title" className="video-upload__label">TITLE YOUR VIDEO</label>
-                <div className="video-upload__field-container">
-                    <textarea 
-                        className="video-upload__title-textarea" 
-                        name="title" 
-                        id="title" 
-                        placeholder="Add a title to your video" 
-                        required>
-                    </textarea>
-                </div>
-
-                <label htmlFor="title" className="video-upload__label">ADD A VIDEO DESCRIPTION</label>
-                <div className="video-upload__field-container">
-                    <textarea 
-                        className="video-upload__description-textarea" 
-                        name="description" 
-                        id="description" 
-                        placeholder="Add a description to your video" 
-                        required>
-                    </textarea>
-                </div>
+            <div className="video-upload__desktop-container">
+                <h1 className='video-upload__heading'>Upload Video</h1>
                 
-                <div className="video-upload__links-container">
-                    <div className="video-upload__publish-button-container">
-                        <button id="publish-button" className="video-upload__publish-button">
-                            PUBLISH
-                        </button>
-                        <img className="video-upload__publish-icon" src={publishIcon} alt="publish icon"/>
+                <form>
+                    <div className="video-upload__desktop-fields-container">
+                        
+                        <div className="video-upload__thumbnail-container"> 
+                            <h4 className='video-upload__label-thumbnail'>VIDEO THUMBNAIL</h4>
+                            <img className='video-upload__thumbnail' src={thumbnail} alt='thumbnail'/>
+                        </div>
+                        
+                        <div className="video-upload__textarea-fields-container">
+                            <div className="video-upload__title-field-container">
+                                <label htmlFor="title" className="video-upload__label-title">TITLE YOUR VIDEO</label>
+                                <textarea 
+                                    className="video-upload__title-textarea" 
+                                    name="title" 
+                                    id="title" 
+                                    placeholder="Add a title to your video" 
+                                    required>
+                                </textarea>
+                            </div>
+                            <div className="video-upload__description-field-container">
+                                <label htmlFor="title" className="video-upload__label-description">ADD A VIDEO DESCRIPTION</label>
+                                <textarea 
+                                    className="video-upload__description-textarea" 
+                                    name="description" 
+                                    id="description" 
+                                    placeholder="Add a description to your video" 
+                                    required>
+                                </textarea>
+                            </div>
+                        </div>
                     </div>
 
-                    <Link className="video-upload__cancel-link" to="/">
-                        <h4 className="video-upload__cancel-text">CANCEL</h4>
-                    </Link>
-                </div>
-            </form> 
+                    <div className="video-upload__links-container">
+                        <div className="video-upload__publish-button-container">
+                            <button id="publish-button" className="video-upload__publish-button">
+                                PUBLISH
+                            </button>
+                            <img className="video-upload__publish-icon" src={publishIcon} alt="publish icon"/>
+                        </div>
 
-            
+                        <Link className="video-upload__cancel-link" to="/">
+                            <h4 className="video-upload__cancel-text">CANCEL</h4>
+                        </Link>
+                    </div>
+                </form> 
+            </div>
         </div>
     )
 };
