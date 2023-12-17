a9beea1f3a33504afebd97f192225329365ba811
david-jiwon-park <davidpark48@hotmail.com>
2023-12-16 22:11:29 -0500

    Creating a separate component for the Video Upload Form within the Video Upload page

---
+++ src/components/VideoUploadForm/VideoUploadForm.js
@@ -0,0 +1,66 @@
+import './VideoUploadForm.scss';
+import thumbnail from '../../assets/images/Upload-video-preview.jpg';
+import publishIcon from '../../assets/images/publish.svg';
+import { Link, useNavigate } from 'react-router-dom';
+
+function VideoUploadForm() {
+
+    const navigate = useNavigate();
+
+    // Creating submit handler that confirms submission and navigates user to home page upon successful submission
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        alert("Your video has been uploaded!");
+        navigate('/');
+    }
+
+    return (  
+        <form onSubmit={handleFormSubmit}>
+            <div className="video-upload-form__desktop-fields-container">
+                
+                <div className="video-upload-form__thumbnail-container"> 
+                    <h4 className='video-upload-form__label-thumbnail'>VIDEO THUMBNAIL</h4>
+                    <img className='video-upload-form__thumbnail' src={thumbnail} alt='thumbnail'/>
+                </div>
+                
+                <div className="video-upload-form__textarea-fields-container">
+                    <div className="video-upload-form__title-field-container">
+                        <label htmlFor="title" className="video-upload-form__label-title">TITLE YOUR VIDEO</label>
+                        <textarea 
+                            className="video-upload-form__title-textarea" 
+                            name="title" 
+                            id="title" 
+                            placeholder="Add a title to your video" 
+                            required>
+                        </textarea>
+                    </div>
+                    <div className="video-upload-form__description-field-container">
+                        <label htmlFor="title" className="video-upload-form__label-description">ADD A VIDEO DESCRIPTION</label>
+                        <textarea 
+                            className="video-upload-form__description-textarea" 
+                            name="description" 
+                            id="description" 
+                            placeholder="Add a description to your video" 
+                            required>
+                        </textarea>
+                    </div>
+                </div>
+            </div>
+
+            <div className="video-upload-form__links-container">
+                <div className="video-upload-form__publish-button-container">
+                    <button type="submit" id="publish-button" className="video-upload-form__publish-button">
+                            PUBLISH
+                    </button>
+                    <img className="video-upload-form__publish-icon" src={publishIcon} alt="publish icon"/>
+                </div>
+
+                <Link className="video-upload-form__cancel-link" to="/">
+                    <h4 className="video-upload-form__cancel-text">CANCEL</h4>
+                </Link>
+            </div>
+        </form> 
+    )
+};
+
+export default VideoUploadForm;
--- src/pages/VideoUploadPage/VideoUploadPage.js
+++ src/pages/VideoUploadPage/VideoUploadPage.js
@@ -1,70 +1,16 @@
 // Page Component for Video Upload Page
 
 import './VideoUploadPage.scss';
-import thumbnail from '../../assets/images/Upload-video-preview.jpg';
-import publishIcon from '../../assets/images/publish.svg';
-import { Link, useNavigate } from 'react-router-dom';
+import VideoUploadForm from '../../components/VideoUploadForm/VideoUploadForm';
 
 function VideoUploadPage() {
-
-    const navigate = useNavigate();
-
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-        alert("Your video has been uploaded!");
-        navigate('/');
-    }
-
     return (
         <div className='video-upload'>
             <div className="video-upload__desktop-container">
                 <h1 className='video-upload__heading'>Upload Video</h1>
                 
-                <form onSubmit={handleFormSubmit}>
-                    <div className="video-upload__desktop-fields-container">
-                        
-                        <div className="video-upload__thumbnail-container"> 
-                            <h4 className='video-upload__label-thumbnail'>VIDEO THUMBNAIL</h4>
-                            <img className='video-upload__thumbnail' src={thumbnail} alt='thumbnail'/>
-                        </div>
-                        
-                        <div className="video-upload__textarea-fields-container">
-                            <div className="video-upload__title-field-container">
-                                <label htmlFor="title" className="video-upload__label-title">TITLE YOUR VIDEO</label>
-                                <textarea 
-                                    className="video-upload__title-textarea" 
-                                    name="title" 
-                                    id="title" 
-                                    placeholder="Add a title to your video" 
-                                    required>
-                                </textarea>
-                            </div>
-                            <div className="video-upload__description-field-container">
-                                <label htmlFor="title" className="video-upload__label-description">ADD A VIDEO DESCRIPTION</label>
-                                <textarea 
-                                    className="video-upload__description-textarea" 
-                                    name="description" 
-                                    id="description" 
-                                    placeholder="Add a description to your video" 
-                                    required>
-                                </textarea>
-                            </div>
-                        </div>
-                    </div>
+                <VideoUploadForm/>
 
-                    <div className="video-upload__links-container">
-                        <div className="video-upload__publish-button-container">
-                            <button type="submit" id="publish-button" className="video-upload__publish-button">
-                                    PUBLISH
-                            </button>
-                            <img className="video-upload__publish-icon" src={publishIcon} alt="publish icon"/>
-                        </div>
-
-                        <Link className="video-upload__cancel-link" to="/">
-                            <h4 className="video-upload__cancel-text">CANCEL</h4>
-                        </Link>
-                    </div>
-                </form> 
             </div>
         </div>
     )
